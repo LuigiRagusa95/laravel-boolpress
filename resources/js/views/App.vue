72bd6bb0dec62c2062f0a85b33571d4bca2dd6a6
LuigiRagusa95 <luigiragusa99@gmail.com>
2022-01-31 15:02:36 +0100

    ADD: app vue component

---
+++ resources/js/views/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Main title</h1>
+    <h2>guest page</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+</style>
--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,5 +1,18 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-sono una guess page
-@endsection
+<head>
+    <meta charset="UTF-8">
+    <title>Front page</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('./css/app.css') }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <div id="root"></div>
+
+    <script src="{{ asset('js/main.js') }}"></script>
+</body>
+
+</html>
