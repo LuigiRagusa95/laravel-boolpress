460d43dd13185cb3508d5e3e5daa13b045e39d90
LuigiRagusa95 <luigiragusa99@gmail.com>
2022-02-01 14:42:10 +0100

    ADD: show the single post

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -4,7 +4,8 @@
 <div class="container">
     <div class="list-group">
         @foreach ($posts as $post)
-        <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+        <a href="{{ route('admin.posts.show', $post->id) }}"
+            class="list-group-item list-group-item-action flex-column align-items-start">
             <div class="d-flex w-100 justify-content-between">
                 <h5 class="mb-1">{{ $post->title }}</h5>
                 <small>{{ $post->created_at->diffForHumans() }}</small>
---
+++ resources/views/admin/posts/show.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <article class="blog p-4">
+                <h1 class="mb-0">{{ $post->title }}</h1>
+                <small class="d-flex align-items-center">
+                    <span class="position-relative">{{ $post->created_at->diffForHumans() }}</span>
+                    <span class="position-relative mb-2 mx-1 font-weight-bold">.</span>
+                    <span class="d-flex position-relative">
+                        <a class="link" href="{{ route('admin.posts.edit', $post->id) }}">Edit</a>
+                    </span>
+                </small>
+                <p class="flex mt-3">{{ $post->text }}</p>
+            </article>
+        </div>
+    </div>
+</div>
+@endsection
