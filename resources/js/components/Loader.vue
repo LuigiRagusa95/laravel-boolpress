07342f1cca20d57db3a4599f40ccb649b7406abd
LuigiRagusa95 <luigiragusa99@gmail.com>
2022-02-08 14:20:32 +0100

    ADD: all posts to guest view with axios callback

---
+++ resources/js/components/Loader.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="loader"></div>
+</template>
+
+<script>
+export default {
+    name: "Loader",
+};
+</script>
+
+<style scoped>
+.loader {
+    width: 24px;
+    height: 24px;
+    border-radius: 999999px;
+    border: 2px solid #c0defb;
+    border-right-color: #3e8ae4;
+}
+</style>
--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,17 +1,54 @@
 <template>
-  <div>
-    <div class="container">
-      <h1>Main title</h1>
-      <a href="/admin">Vai alla pagina admin</a>
+    <div>
+        <div class="container">
+            <h1 class="mt-2">Posts</h1>
+            <section class="px-5 py-2">
+                <article
+                    class="mb-5"
+                    v-for="post in posts"
+                    :key="`post-${post.id}`"
+                >
+                    <h1 class="h3">{{ post.title }}</h1>
+                    <p class="text-small">{{ post.created_at }}</p>
+                    <p class="text-normal">{{ post.text }}</p>
+                </article>
+            </section>
+            <!-- <a href="/admin">Vai alla pagina admin</a> -->
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
+import axios from "axios";
+import Loader from "../components/Loader.vue";
+
 export default {
-  name: "App",
+    name: "App",
+    data() {
+        return {
+            posts: null,
+        };
+    },
+    created() {
+        this.getPosts();
+    },
+    methods: {
+        getPosts() {
+            axios
+                .get("http://127.0.0.1:8000/api/posts")
+                .then((res) => (this.posts = res.data));
+        },
+    },
 };
 </script>
 
 <style>
+.text-small {
+    font-size: 12px;
+    line-height: 16px;
+}
+.text-normal {
+    font-size: 16px;
+    line-height: 24px;
+}
 </style>
