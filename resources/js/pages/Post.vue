14dd9d6d4e74a4050d94558f74981821cc96d01a
LuigiRagusa95 <luigiragusa99@gmail.com>
2022-02-10 15:50:44 +0100

    ADD: go back to posts

--- resources/js/pages/Post.vue
+++ resources/js/pages/Post.vue
@@ -3,7 +3,12 @@
         <section class="mb-3 px-5 py-2">
             <div v-if="!post" class="loader-container"><Loader /></div>
             <article v-else class="mb-5">
-                <h1 class="h3 m-0">{{ post.title }}</h1>
+                <div class="d-flex align-items-center mb-3">
+                    <a class="icon" @click="$router.go(-1)">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="none" stroke="currentColor" stroke-linecap="square" stroke-miterlimit="10" stroke-width="48" d="M244 400L100 256l144-144M120 256h292"/></svg>
+                    </a>
+                    <h1 class="h3 m-0 ml-2">{{ post.title }}</h1>
+                </div>
                 <span class="text-small">{{ formatDate(post.created_at) }}</span>
                 <Tags class="mb-2" :list="post.tags" />
                 <p class="text-normal mt-3">{{ post.text }}</p>
@@ -49,5 +54,12 @@ export default {
 </script>
 
 <style>
-
+.icon {
+    width: 24px;
+    height: 24px;
+    display: flex;
+    cursor: pointer;
+    align-items: center;
+    justify-content: center;
+}
 </style>
