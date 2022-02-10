0c4116ec40b88eb49b9abeeb6a7f17518798b130
LuigiRagusa95 <luigiragusa99@gmail.com>
2022-02-10 13:42:43 +0100

    ADD: vue router its confing and home page

---
+++ resources/js/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <header></header>
+        <main>
+            <router-view></router-view>
+        </main>
+        <footer></footer>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App",
+};
+</script>
+
+<style></style>
--- resources/js/main.js
+++ resources/js/main.js
@@ -1,7 +1,9 @@
 import Vue from "vue";
-import App from './views/App.vue';
+import App from './App.vue';
+import router from "./routes";
 
 const root = new Vue({
     el: '#root',
+    router: router,
     render: h => h(App)
 });
