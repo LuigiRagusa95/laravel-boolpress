323a1c8c1055bdd835e3b961ed49362830e8ca76
LuigiRagusa95 <luigiragusa99@gmail.com>
2022-02-14 15:31:53 +0100

    ADD: send welcome mail

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Mail\SendWelcomeMail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
 class HomeController extends Controller
 {
     public function index()
     {
+        Mail::to(Auth::user()->email)->send(new SendWelcomeMail(Auth::user()->name));
         return view('admin.home');
     }
 }
